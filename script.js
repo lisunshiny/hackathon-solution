594f2937316cf604f6e9d5ee1a6a00d307407e72
Liann Sun <lisunshiny@gmail.com>
2017-01-23 00:42:00 -0500

    add all other files

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   $("button").on("click", function(event) {
     var val = $("input").val();
 
-    $.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + val + "&type=grocery_or_supermarket&key=AIzaSyAp_xGN8rsxtH80RUiG1Hq07_4QalYvBBs", function(response) {
+    $.get("http://hackathon.liannsun.com/supermarkets?secret_key=spacecats&location=" + val, function(response) {
       $(".results-container").empty()
       var results = response.results;
       $results = $("<div>")
       $list = $("<ul>");
 
-      $results.append("wow there are <b>" + results.length + "</b> grocery stores near you!")
+      $results.append("wow there are <b>" + results.length + "</b> grocery stores near you!</br>")
 
       for (var i = 0; i < results.length; i++) {
         var name = results[i].name;
